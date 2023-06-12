661d0f3e44139c5200bdb5031b30feefa8789427
ilyosbek9531 <iqboljonsheraliyev11@gmail.com>
2023-06-13 00:54:09 +0500

    api added

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const fileUpload = require("express-fileupload");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(fileUpload());
 app.use("/uploads", express.static("uploads")); // Serve the static files from the "uploads" directory
+app.use(bodyParser.json());
 
 app.post("/upload", function (req, res) {
   if (!req.files || !req.files.image) {
@@ -23,6 +25,13 @@ app.post("/upload", function (req, res) {
       req.protocol + "://" + req.get("host") + "/uploads/" + file.name;
     res.status(200).json({ message: "File uploaded successfully.", imageUrl });
   });
+});
+
+app.get("/", (req, res) => {
+  res.status(200).json([
+    { id: 1, name: "ilyosbek" },
+    { id: 2, name: "alisher" },
+  ]);
 });
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
